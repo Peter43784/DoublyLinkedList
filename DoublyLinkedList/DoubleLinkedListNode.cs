203f87c827a0f136edcaf90f287ead01c0055c9a
KYIV\Petro_Vozhdai <Petro_Vozhdai@epam.com>
2016-12-18 23:21:23 +0200

    add Previous, Next nodes

--- DoublyLinkedList/DoubleLinkedListNode.cs
+++ DoublyLinkedList/DoubleLinkedListNode.cs
@@ -3,6 +3,8 @@
     public class DoubleLinkedListNode<T>
     {
         public T Value { get; private set; }
+        public DoubleLinkedListNode<T> Previous { get; set; }
+        public DoubleLinkedListNode<T> Next { get; set; }
 
         public DoubleLinkedListNode(T value)
         {
