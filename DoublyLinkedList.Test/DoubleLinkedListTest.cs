5e436ef5693887b03c708174a535a01a17570cbc
KYIV\Petro_Vozhdai <Petro_Vozhdai@epam.com>
2016-12-18 23:18:29 +0200

    add previous/next nodes tests

--- DoublyLinkedList.Test/DoubleLinkedListTest.cs
+++ DoublyLinkedList.Test/DoubleLinkedListTest.cs
@@ -21,5 +21,28 @@ namespace DoublyLinkedList.Test
 
             Assert.AreEqual(value, node.Value);
         }
+
+        [Test]
+        public void DoubleLinkedListNode__CorrectPreviousNode()
+        {
+            var node = new DoubleLinkedListNode<int>(1);
+            var previousNode = new DoubleLinkedListNode<int>(2);
+
+            node.Previous = previousNode;
+
+            Assert.AreEqual(node.Previous, previousNode);
+        }
+
+        [Test]
+        public void NewDoubleLinkedListNode_CorrectNextNode()
+        {
+            var node = new DoubleLinkedListNode<int>(1);
+            var nextNode = new DoubleLinkedListNode<int>(2);
+
+            node.Next = nextNode;
+
+            Assert.AreEqual(node.Next, nextNode);
+        }
+
     }
 }
