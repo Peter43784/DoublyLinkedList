fd634ea995302cd6a406e47a7fee733f9d52f083
KYIV\Petro_Vozhdai <Petro_Vozhdai@epam.com>
2016-12-18 22:50:55 +0200

    Add test that checks if list's node contains right value

---
+++ DoublyLinkedList.Test/DoubleLinkedListTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DoublyLinkedList.Test
+{
+    [TestFixture]
+    internal class DoubleLinkedListTest
+    {
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void NewDoubleLinkedListNode_ReturnsCorrectValue(int number)
+        {
+            var node = new DoubleLinkedListNode(number);
+
+            Assert.AreEqual(number, node.value);
+        }
+    }
+}
