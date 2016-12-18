04ec411bc11a5b0f8a512061098b7eda826db179
KYIV\Petro_Vozhdai <Petro_Vozhdai@epam.com>
2016-12-19 00:24:37 +0200

    Add tests for addFirst and AddLast methods

---
+++ DoublyLinkedList.Test/DoubleLinkedListTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace DoublyLinkedList.Test
+{
+    [TestFixture]
+    internal class DoubleLinkedListTest
+    {
+
+        [TestCase(1)]
+        public void DoubleLinkedList_AddFirst_ReturnsCorrectElement(int value)
+        {
+            IDoubleLinkedList<T> list = new DoubleLinkedList<T>();
+
+            list.AddFirst(1);
+
+            Assert.AreEqual(value, list.First);
+        }
+
+        [TestCase(1)]
+        public void DoubleLinkedList_AddLast_ReturnsCorrectElement(int value)
+        {
+            IDoubleLinkedList<T> list = new DoubleLinkedList<T>();
+
+            list.AddLast(1);
+
+            Assert.AreEqual(value, list.Last);
+
+        }
+
+    }
+}
